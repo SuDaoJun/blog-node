3e71615ecae2d9158d88b4621d6b14d017680b72
sudaojun <1251662462@qq.com>
2020-08-24 20:23:42 +0800

    .

--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const  msg = {
-  name: 'admin',
-  pwd: '123456',
+  name: 'xxx',
+  pwd: 'xxx',
   dbName: 'blogNode'
 }
 const mongodbURI = `mongodb://${msg.name}:${msg.pwd}@localhost:27017/${msg.dbName}`
